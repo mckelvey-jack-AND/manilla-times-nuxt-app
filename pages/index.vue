e5ced2b2d805df245b18c08aa3a242dbb87d400b
Jack McKelvey <jack.mckelvey@and.digital>
2024-02-28 11:32:20 +0000

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <TestComponent />
+    <p>{{ data }}</p>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch("/api/test");
+</script>
