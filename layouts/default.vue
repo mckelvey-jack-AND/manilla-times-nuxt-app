51a40366aeaf6860848724795751b053891584ac
Jack McKelvey <jack.mckelvey@and.digital>
2024-04-17 11:32:27 +0100

    chore: add seo data

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,12 @@
 <template>
+  <Head>
+    <Title>Manilla Times Band</Title>
+    <Meta
+      name="description"
+      content="Hailing from small idyllic towns in North Wales (Mold) and Ireland (Derrylin) respectively, Manilla Times are an emerging modern indie rock band, known for their energetic live set, and catchy indie anthems."
+    />
+    <Style type="text/css" children="body { background-color: green; }"></Style>
+  </Head>
   <NavBar />
   <main>
     <slot />
