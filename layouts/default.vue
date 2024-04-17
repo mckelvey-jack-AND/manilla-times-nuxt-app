b09ad8e1401f0424ba82b58983db5f0b4c3605d4
Jack McKelvey <jack.mckelvey@and.digital>
2024-04-17 11:37:23 +0100

    remove unused css name

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,6 @@
       name="description"
       content="Hailing from small idyllic towns in North Wales (Mold) and Ireland (Derrylin) respectively, Manilla Times are an emerging modern indie rock band, known for their energetic live set, and catchy indie anthems."
     />
-    <Style type="text/css" children="body { background-color: green; }"></Style>
   </Head>
   <NavBar />
   <main>
