785b0143519abd64d98a16ba99f2bf30b4643aee
Jack McKelvey <jack.mckelvey@and.digital>
2024-04-17 10:45:54 +0100

    chore: add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/base.css"],
   ssr: false,
-  modules: [["@storyblok/nuxt", { accessToken: "CcZSZxUrJQ7JRTs5NjxenQtt" }]],
+  modules: [
+    ["@storyblok/nuxt", { accessToken: "CcZSZxUrJQ7JRTs5NjxenQtt" }],
+    "@nuxtjs/sitemap",
+  ],
   runtimeConfig: {
     public: {
       gtagId: "GTM-PMZH2BGG",
