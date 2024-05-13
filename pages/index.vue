0572c7821f4160a87c6b2f521e7222915ba10c88
Jack McKelvey <jack.mckelvey@and.digital>
2024-05-13 20:55:46 +0100

    chore add description

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <Title>Manilla Times</Title>
+  <Meta
+    name="description"
+    content="Hailing from small idyllic towns in North Wales and Ireland, Manilla Times are an emerging modern indie rock band, known for their energetic live set, and catchy indie anthems."
+  />
   <StoryblokComponent v-if="story" :blok="story.content" />
   <Script
     async
