77baf088e29fc26e6182754a6ef1b6631051df5c
Jack McKelvey <jack.mckelvey@and.digital>
2024-05-23 14:17:54 +0100

    add gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,39 +2,18 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/base.css"],
-  plugins: ["~/plugins/ga.js"],
-  ssr: true,
+  ssr: false,
   modules: [
     ["@storyblok/nuxt", { accessToken: "CcZSZxUrJQ7JRTs5NjxenQtt" }],
     "@nuxtjs/sitemap",
+    "nuxt-gtag",
   ],
+  gtag: {
+    id: "G-VJYFTT5DM5",
+  },
   runtimeConfig: {
     public: {
       gtagId: "GTM-PMZH2BGG",
-    },
-  },
-  app: {
-    head: {
-      script: [
-        {
-          src: `https://www.googletagmanager.com/gtag/js?id=YOUR_GA_MEASUREMENT_ID`,
-          async: true,
-        },
-        {
-          hid: "gtag-init",
-          innerHTML: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'YOUR_GA_MEASUREMENT_ID', { send_page_view: false });
-          `,
-          type: "text/javascript",
-          charset: "utf-8",
-        },
-      ],
-      __dangerouslyDisableSanitizersByTagID: {
-        "gtag-init": ["innerHTML"],
-      },
     },
   },
 });
