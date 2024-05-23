1d6c28623bff8355a4eeb6ba83f278a82c462c3e
Jack McKelvey <jack.mckelvey@and.digital>
2024-05-23 12:15:11 +0100

    update seo

--- pages/index.vue
+++ pages/index.vue
@@ -11,11 +11,12 @@
   </Script>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const story = await useAsyncStoryblok("home", { version: "draft" });
 useSeoMeta({
   title: "Manilla Times Band",
   ogTitle: "Manilla Times Band",
+  ogImage: "https://www.manillatimesband.com/MT_logo_white_logo.png",
   description:
     "Hailing from small idyllic towns in North Wales and Ireland, Manilla Times are an emerging modern indie rock band, known for their energetic live set, and catchy indie anthems.",
   ogDescription:
