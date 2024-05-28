dd924bc3419dfbff67165e00864eb14f2d24bbd8
Jack McKelvey <jack.mckelvey@and.digital>
2024-05-28 11:39:05 +0100

    remove schema data

--- pages/index.vue
+++ pages/index.vue
@@ -14,32 +14,4 @@ useSeoMeta({
   ogDescription:
     "Hailing from small idyllic towns in North Wales and Ireland, Manilla Times are an emerging modern indie rock band, known for their energetic live set, and catchy indie anthems.",
 });
-
-const jsonld = {
-  "@context": "https://schema.org",
-  "@type": "MusicGroup",
-  name: "Manilla Times",
-  url: "https://www.manillatimesband.com",
-  image: "https://www.manillatimesband.com/MT_logo_white_logo.png",
-  genre: "Indie Rock",
-  track: [
-    {
-      "@type": "MusicRecording",
-      name: "Third Time Lucky",
-      url: "https://open.spotify.com/track/49HgyKaneQrj1YeJp6OvUC?si=62a97b96b9b24208",
-      duration: "PT3M6S",
-    },
-  ],
-  description:
-    "Hailing from small idyllic towns in North Wales and Ireland, Manilla Times are an emerging modern indie rock band, known for their energetic live set, and catchy indie anthems.",
-};
-useHead({
-  script: [
-    {
-      hid: "breadcrumbs-json-ld",
-      type: "application/ld+json",
-      textContent: JSON.stringify(jsonld),
-    },
-  ],
-});
 </script>
