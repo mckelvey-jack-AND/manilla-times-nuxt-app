c90d937a37e73537da09b2f78805c70c6c31e6b2
Jack McKelvey <jack.mckelvey@and.digital>
2025-02-25 20:13:22 +0000

    chore: add error page

---
+++ error.vue
@@ -0,0 +1,32 @@
+<template>
+  <NavBar />
+  <main>
+    <div class="error-image-container">
+      <img src="/assets/404.jpg" />
+    </div>
+  </main>
+  <FooterComponent />
+</template>
+<script setup>
+import NavBar from "~/components/NavBar.vue";
+import FooterComponent from "~/components/FooterComponent.vue";
+</script>
+
+<style scoped>
+h1 {
+  margin-top: 24px;
+  text-align: center;
+  font-size: 32px;
+  text-transform: uppercase;
+}
+
+.error-image-container {
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  width: 100%;
+  max-width: 600px;
+}
+</style>
